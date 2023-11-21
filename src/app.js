70419046c080cae832bc9502cf4d3f662b4acede
Moises Correa <145796144+MoicoeMT@users.noreply.github.com>
2023-11-21 06:15:52 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,43 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const cards = [
+  "A",
+  "J",
+  "Q",
+  "K",
+  "10",
+  "9",
+  "8",
+  "7",
+  "6",
+  "5",
+  "4",
+  "3",
+  "2"
+];
+const pintas = [
+  'fa-solid fa-heart" style="color: #ff1100;',
+  'fa-solid fa-diamond" style="color: #fc0303;',
+  'fa-solid fa-clover style="color: black;',
+  'bi bi-suit-spade-fill style="color: black;'
+];
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let ourCard = document.getElementById("carta");
+  let randomNum = Math.floor(Math.random() * cards.length);
+  let randomIcon = Math.floor(Math.random() * pintas.length);
+  let iconCard = cards[randomNum];
+  let figure = pintas[randomIcon];
+
+  let ourHtml = `<div class="justify-content-start pt-2 display-4">
+  <i class="${figure}"></i>
+  </div>
+  <div class="py-5 d-flex justify-content-center my-5">
+  <h1 class="" id="titulo">${iconCard}</h1>
+  </div>
+  <div class="d-flex justify-content-end pb-2 display-4">
+  <i class="fa-rotate-180 ${figure}"></i>
+  </div>`;
+  ourCard.innerHTML = ourHtml;
 };
